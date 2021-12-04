435645e8def5f838056c68b1e17f840f5962abf3
savelgmx <savel@gmx.com>
2021-12-04 18:17:40 +0700

    Builind App //исправить ошибки при билде

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.viewmodel.WeatherViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: WeatherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        viewModel.weatherResponse.observe(this, { weather ->
+
+            binding.apply {
+/*
+                tvCityName.text = "Krasnoyrask"
+                tvDescription.text = weather.description
+                tvTemperature.text = weather.temperature
+                tvWind.text = weather.wind
+
+                val forecast = weather.forecast
+                tvForecast1.text = "${forecast[0].temperature}/ ${forecast[0].wind}"
+                tvForecast2.text = "${forecast[1].temperature}/ ${forecast[1].wind}"
+                tvForecast3.text = "${forecast[2].temperature}/ ${forecast[2].wind}"
+*/
+
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.weatherapp.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.model.Weather
+import com.example.weatherapp.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel
+@Inject
+constructor(private val repository: WeatherRepository) : ViewModel() {
+
+    private val _response = MutableLiveData<Weather>()
+    val weatherResponse: LiveData<Weather>
+        get() = _response
+
+
+    init {
+        getWeather()
+    }
+
+    private fun getWeather() = viewModelScope.launch {
+        repository.getWeather().let { response ->
+
+            if (response.isSuccessful) {
+                _response.postValue(response.body())
+            } else {
+                Log.d("tag", "getWeather Error: ${response.code()}")
+            }
+        }
+    }
+
+
+}
+
