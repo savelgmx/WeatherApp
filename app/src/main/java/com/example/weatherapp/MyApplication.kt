a9165e744bf6538276a10b940bf702de896849e6
savelgmx <savel@gmx.com>
2022-01-05 17:27:46 +0700

    removed RuntimeException: Unable to instantiate application com.example.weatherapp.MyApplication: java.lang.ClassCastException: com.example .weatherapp.MyApplication

--- app/src/main/java/com/example/weatherapp/MyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-class MyApplication {
-    @HiltAndroidApp
-    class MyApplication : Application()
-}
